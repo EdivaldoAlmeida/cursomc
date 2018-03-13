1014c16adaedc1d22e8397d9159cb5b642a54eb5
Edivaldo Almedeida <edivaldo.va@gmail.com>
2018-03-13 14:05:30 -0300

    Testanto a prieira classe de domínio - Categoria

--- src/main/java/com/edivaldova/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edivaldova/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.edivaldova.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edivaldova.cursomc.domain.Categoria;
 
 @RestController //Essa classe (CategoriaResource) será um controlador Rest
 /*
@@ -15,13 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
  * --> Interface uniforme -
  * --> Sistema em camadas - Possibilidade de adicionar mais servidores em uma aplicação. Cliente não sabe qual servidor está lhe reespondendo
  * --> Código sob demanda - Aplicação poder evoluir para adaptar o cliente de acordo com novos requisitos e funcionalidades  
- * 
+ * 	
  */
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
+	/*O método abaixo - listar() - é o responsável por responder pelo GET no recurso /categorias
+	 * ele irá retornar uma lista de categorias
+	 * 
+	 */
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "Rest está funcionando!";
+	public List<Categoria> listar(){
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2,  "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>(); // List é uma interface que pode ser implementada por ArrayList que é uma classe
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 }
