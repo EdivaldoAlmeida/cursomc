fb8b8afefc0f1b5ffd305199d74407457842f3f3
Edivaldo Almedeida <edivaldo.va@gmail.com>
2018-05-03 13:38:18 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/edivaldova/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/edivaldova/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.edivaldova.cursomc.services.exceptions.DataIntegrityException;
 import com.edivaldova.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -14,7 +15,7 @@ import com.edivaldova.cursomc.services.exceptions.ObjectNotFoundException;
 public class ResourceExceptionHandler {
 	//Classe manipuladora de exceções do recurso.
 
-	//Essa anotation informa que esse é um tratador de exceção do tipo que está entre parêntreses
+	//Essa anotation informa que esse é um tratador de exceção do tipo que está entre parêntreses. Nesse caso, de não encontrado
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> object(ObjectNotFoundException e,HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
@@ -22,4 +23,13 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	
 	}
+	
+	//Essa anotation informa que esse é um tratador de exceção do tipo de violação de 
+	//integridade não permitindo a exclusão de catetoria que possui produto. 
+	@ExceptionHandler(DataIntegrityException.class)
+		public ResponseEntity<StandardError> DataIntegrity(DataIntegrityException e,HttpServletRequest request){
+			StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+}
 }
--- src/main/java/com/edivaldova/cursomc/services/CategoriaService.java
+++ src/main/java/com/edivaldova/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.edivaldova.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.edivaldova.cursomc.domain.Categoria;
 import com.edivaldova.cursomc.repositories.CategoriaRepository;
+import com.edivaldova.cursomc.services.exceptions.DataIntegrityException;
 import com.edivaldova.cursomc.services.exceptions.ObjectNotFoundException;
 
 /* Implementando o serviço que busca uma categoria 
@@ -38,5 +40,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {	
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+		
 	}
 }
