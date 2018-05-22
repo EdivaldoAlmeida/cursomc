389d5ac44019982e7b88743f1acb42853a37c32c
Edivaldo Almedeida <edivaldo.va@gmail.com>
2018-05-22 09:50:14 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/edivaldova/cursomc/resources/ClienteResource.java
+++ src/main/java/com/edivaldova/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.edivaldova.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edivaldova.cursomc.domain.Cliente;
+import com.edivaldova.cursomc.dto.ClienteDTO;
 import com.edivaldova.cursomc.services.ClienteService;
 
 @RestController //Essa classe (ClienteResource) será um controlador Rest
@@ -39,4 +48,50 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	
+	//Atualizavalondo uma categoria no BD
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
+		Cliente obj = service.fromDTO(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+	//Endpoint para mostrar todas as categorias.
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll(){			
+		List<Cliente> list = service.findAll();		
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
+		/* para cada objeto desta lista tem q instanciar o dto correspondente
+		 * STREAM é um recurso do JAVA 8. MAP faz uma operação para cada objeto da lista.
+		 * -> é uma função anônima que recebe um obj e cria uma nova ClienteDTO e passa esse objeto como argumento.
+		 * Depois volta esse obj para o tipo lista utilizando o Collectors.toList. Resumindo transforma uma lista em outra... 			 */
+	}
+	
+	
+	@RequestMapping(value="/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> fidPage(
+			@RequestParam(value="page",defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage",defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy",defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction",defaultValue="ASC")String direction){				
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);		
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	
+	
 }
--- src/main/java/com/edivaldova/cursomc/services/ClienteService.java
+++ src/main/java/com/edivaldova/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.edivaldova.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.edivaldova.cursomc.domain.Cliente;
+import com.edivaldova.cursomc.dto.ClienteDTO;
 import com.edivaldova.cursomc.repositories.ClienteRepository;
+import com.edivaldova.cursomc.services.exceptions.DataIntegrityException;
 import com.edivaldova.cursomc.services.exceptions.ObjectNotFoundException;
 
 /* Implementando o serviço que busca um cliente 
@@ -28,7 +35,48 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: "+
 		Cliente.class.getName()));
+			
+	}
+	
+	//Esse update permitirá atualizar os dados de um cliente com base nos dados já cadastrados desse cliente, por exemplo
+	//Caso se queira alterar o nome do cliente este udate irá pegar os dados antigos (cpf, endereço, etc) e irá inserir nesse
+	//cliente atualizado, senão ele iria retornar nula para esses dados.
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {	
+		repo.deleteById(id);
+		}catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir um cliente porque há entidades relacionadas");
+		}
 		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	//Método que mostra as categorias em forma de paginação, de acordo com as carecterísticas informadas.
+	//Page encapsula informações e operações sobre encapsulação.
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+	
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+		
+	}
+	//Atualiza os dados do cliente advindos do objeto obj
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
